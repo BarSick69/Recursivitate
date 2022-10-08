b5368a539ef3c0f829adcafab34edc99d03e2460
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2022-10-08 12:25:45 +0300

    Update cmmdc2REC.cpp

--- cmmdc2REC.cpp
+++ cmmdc2REC.cpp
@@ -1,21 +1,24 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
-void cmmdc(int a,int b);
+void cmmdc(int a,int b,int &r);
 int main() {
     // Write C++ code here
-    cmmdc(24,32);
+    int r;
+    cmmdc(48,18,r);
+    cout<<r;
     return 0;
 }
 
-void cmmdc(int a,int b){
+void cmmdc(int a,int b,int &r){
     if(a>b){
-        cmmdc(a-b,b);
+        cmmdc(a-b,b,r);
     }
     else if(b>a){
-        cmmdc(a,b-a);
+        cmmdc(a,b-a,r);
     }
     else{
-        cout<<a;
+        r=a;
+        
     }
 }
