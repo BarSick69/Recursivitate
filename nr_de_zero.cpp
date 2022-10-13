48454e36c816be3b10d07a6208fab87c8797b1e2
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2022-10-13 19:34:39 +0300

    Update nr_de_zero.cpp

--- nr_de_zero.cpp
+++ nr_de_zero.cpp
@@ -1,20 +1,25 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
-void nr_de_zero(int n);
-int zero=0;
+void zero(int n,int &zeros);
 int main() {
+    int n = 100010110;
+    int zeros=0;
+    zero(n,zeros);
     // Write C++ code here
-    nr_de_zero(12400010);
+    cout << zeros;
+
     return 0;
 }
 
-void nr_de_zero(int n){
-    if(n%10==0){
-        zero+=1;
-        nr_de_zero(n/10);
-    }
-    else{
-        cout<<zero;
+void zero(int n,int &zeros){
+    if(n!=0){
+        if(n%10==0){
+            zeros+=1;
+            zero(n/10,zeros);
+        }
+        else{
+            zero(n/10,zeros);
+        }
     }
 }
