ccd68d2cb30522d9ec47141829eff41e64e26c43
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2022-10-13 20:20:03 +0300

    Update 824CifMaxRec.cpp

--- 824CifMaxRec.cpp
+++ 824CifMaxRec.cpp
@@ -1,9 +1,8 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 void cifmax(int num,int &max);
 int main() {;
-    int num=200754;
+    int num=1252598246;
     int max=0;
     cifmax(num,max);
     cout<<max;
@@ -11,8 +10,11 @@ int main() {;
 }
 
 void cifmax(int num,int &max){
-    if(num%10>max){
+    if(num%10>max&&num!=0){
         max=num%10;
+        cifmax(num/10,max);
+    }
+    else if(num!=0){
         cifmax(num/10,max);
     }
 }
